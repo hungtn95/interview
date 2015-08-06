aa501e6ce3623222ecaf60207b62a32770fe9813
Richard <richard.garbi@gmail.com>
2015-08-06 15:01:13 -0500

    Problem set

---
+++ problems/src/main/java/com/payit/problems/Problem3.java
@@ -0,0 +1,28 @@
+package com.payit.problems;
+
+/**
+ * We have a list of customers and we are fairly certain
+ * that there are duplicates in the list.
+ *
+ * You must count the number of duplicates and possible duplicates.
+ *
+ * A duplicate is a customer where their first name, last name, and email address all match.
+ * A possible duplicate is where their first name and last name match but they have different email addresses.
+ *
+ * The list of customers is found in the json document 'customers.json' found in the resources directory.
+ * You must load and deserialize the document into a list of Customer objects. (The customer object is found in the models package)
+ * Feel free to use any open source library you would like to aid you in the deserialization. (Hint: Gradle is your friend)
+ * Once you have a list of customer objects count the number of duplicates and the number of possible duplicates.
+ * Print your totals on the console.
+ *
+ * Other Notes:
+ * A matching pair is considered 1 duplicate. Three of the same is considered two matches. (i.e. N - 1)
+ *
+ */
+public class Problem3 {
+
+    public void findDuplicates() {
+
+    }
+
+}
---
+++ problems/src/test/java/com/payit/problems/Problem3Test.java
@@ -0,0 +1,18 @@
+package com.payit.problems;
+
+import org.junit.Test;
+
+
+/**
+ * Created by Richard on 8/6/15.
+ */
+public class Problem3Test {
+
+    @Test
+    public void customerDuplicatesTest() {
+        Problem3 problem3 = new Problem3();
+        problem3.findDuplicates();
+    }
+
+
+}
